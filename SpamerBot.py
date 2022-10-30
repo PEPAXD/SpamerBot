7c2fa8ef30486b69a9e511ab9087c6621e0c08d9
PEPAXD <107534710+PEPAXD@users.noreply.github.com>
2022-10-30 17:02:07 -0300

    update v0.2 SpamerBot
    Add input emails and user target to send multiple users

--- SpamerBot.py
+++ SpamerBot.py
@@ -8,42 +8,71 @@ def printCodeMaster():
     RED = '\033[31m'
     RESET = '\033[39m'
 
-    print(RED + "\n EMAIL-SEND BY MAURO PEPA. V0.1"+ "\n--------------------------------" + RESET)
+    print(RED + "\n EMAIL-SEND BY MAURO PEPA. V0.2"+ "\n--------------------------------" + RESET)
+
+#FUNCION ADD TARGET EMAILS USERS
+def targetUser():
+
+    name_list = []
+    email_list = []
+    name = None
+
+    print("PRESS X TO FINISH AND SEND EMAILS")
+    while name != "x":
+
+        name = input("\nNombre usuario: ")
+
+        if(name == "x"):
+            break
+        else:
+            name_list.append(name)
+
+        email = input("Email usuario: ")
+        email_list.append(email)
+
+    return email_list
 
 #FUNCION LOGGEAR CORREO Y ENVIAR CORREO
-def signInEmail():
+def signInEmail(email_list):
 
     # CUENTA GOOGLE EMISOR
     email_emisor = 'testsendvcp1@gmail.com'
     email_contrasena_ia = 'ytfgrmnsbfxnhvgp'
     # CONTRASEÑA EMAIL USUARIO = rejfxjufnvrl
 
-    # CUENTAS GOOGLE RECEPTOR
-    email_receptor = '' #EMAIL TARGET
-
     # DOCUMENTOS A ENVIAR
     asunto = 'FUNCIONA!!!'
-    cuerpo = """El codigo funciona, INGRESE AL SIGUIENTE LINK ---> https://www.instagram.com/ambientevcp/?hl=es"""
-
-    em = EmailMessage()         # CREO OBJETO
-    em['From'] = email_emisor   # EMISOR
-    em['To'] = email_receptor   # RECEPTOR
-    em['Subject'] = asunto      # ASUNTO
-    em.set_content(cuerpo)      # CUERPO DEL CORREO
+    cuerpo = """INGRESE AL SIGUIENTE LINK ---> https://www.instagram.com/ambientevcp/?hl=es"""
 
     # CHEQUEO DE SEGURIDAD
     contexto = ssl.create_default_context()
 
-    # CONECCION PYTHON CON CORREO EMISOR, N°PUERTO, PARAMETRO SEGURIDAD
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
-        smtp.login(email_emisor, email_contrasena_ia)  # FUNCION LOGEARSE
-        smtp.sendmail(email_emisor, email_receptor, em.as_string())  # FUNCION ENVIAR CORREO
+    count = 0
+    while count<len(email_list):
+
+        # CREO OBJETO
+        em = EmailMessage()
+        em['From'] = email_emisor   # EMISOR
+        em['Subject'] = asunto      # ASUNTO
+        em.set_content(cuerpo)      # CUERPO DEL CORREO
+
+        # CUENTAS GOOGLE RECEPTOR
+        email_receptor = email_list[count]
+        em['To'] = email_receptor   # RECEPTOR
+
+        # CONECCION PYTHON CON CORREO EMISOR, N°PUERTO, PARAMETRO SEGURIDAD
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
+            smtp.login(email_emisor, email_contrasena_ia)  # FUNCION LOGEARSE
+            smtp.sendmail(email_emisor, email_receptor, em.as_string())  # FUNCION ENVIAR CORREO
+
+        count += 1
 
 
 #FUNCION MAIN
 def main():
     printCodeMaster()
-    signInEmail()
+    email_list = targetUser()
+    signInEmail(email_list)
 
 #EJECUTAR SCRIPT
 if __name__ == '__main__':
